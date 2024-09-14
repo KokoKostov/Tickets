a5803c1038affb556311710febcc8a05bc448671
KokoKostov <127635232+KokoKostov@users.noreply.github.com>
2024-09-14 16:23:11 +0300

    hide the add to cart button on bought tickets

--- ticket-web/src/components/Details/Details.js
+++ ticket-web/src/components/Details/Details.js
@@ -2,32 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { api } from '../../api';
 import { useCart } from '../../providers/CartProvider';
+import { useAuth } from '../../providers/AuthProvider';
 
 function Details() {
     const navigate = useNavigate();
-    const [userDetails, setUserDetails] = useState(null);
     const [ticket, setTicket] = useState(null);
     const { id } = useParams();
-    const {addToCart,cart} = useCart()
-    
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await api.get('/me');
-                setUserDetails(response.data);
-            } catch (error) {
-                console.error('Failed to fetch user data:', error);
-            }
-        };
-        fetchUserData();
-    }, []);
+    const { addToCart, cart } = useCart();
+    const { user } = useAuth();
 
-    
     useEffect(() => {
         const fetchTicket = async () => {
             try {
                 const response = await api.get(`/tickets/${id}`);
                 setTicket(response.data);
+                
             } catch (error) {
                 console.error('Failed to fetch ticket:', error);
             }
@@ -46,23 +35,22 @@ function Details() {
             }
         }
     };
-    const addHandler= ()=>{
-        if(cart.some((cartItem=> cartItem._id === ticket._id))){
-            return window.alert('This item is already in your cart')
-           
-        }
-        console.log(userDetails);
-        
 
-        if(!userDetails){
-            window.alert('You need to Login to buy a ticket')
-            navigate('/login')
-            return
+    const addHandler = () => {
+        if (cart.some((cartItem) => cartItem._id === ticket._id)) {
+            return window.alert('This item is already in your cart');
         }
-        addToCart(ticket)
-        window.alert('Ticket has been added to your cart')
-        navigate('/catalog')
-    }
+
+        if (!user) {
+            window.alert('You need to log in to buy a ticket');
+            navigate('/login');
+            return;
+        }
+        
+        addToCart(ticket);
+        window.alert('Ticket has been added to your cart');
+        navigate('/catalog');
+    };
 
     if (!ticket) {
         return <p>No ticket details available</p>;
@@ -93,7 +81,9 @@ function Details() {
                             <p>Event description:</p>
                             <p>{ticket.description}</p>
                         </div>
-                        {userDetails && ticket.user === userDetails._id ? (
+
+                        {/* Conditional rendering for ticket actions */}
+                        {user && ticket.user === user._id ? (
                             <div className='flex items-center justify-between m-4 align-bottom'>
                                 <Link to={`/tickets/edit/${ticket._id}`}>
                                     <button className='bg-green-600 text-xl flex items-center justify-center transform hover:-translate-y-1 hover:scale-110'>
@@ -105,11 +95,16 @@ function Details() {
                                 </button>
                             </div>
                         ) : (
-                            <div className='flex items-center justify-center m-4'>
-                                <button onClick={addHandler} className='bg-green-600 text-xl flex items-center justify-center transform hover:-translate-y-1 hover:scale-110'>
-                                    Add to cart
-                                </button>
-                            </div>
+                           
+                            ticket.buyer.includes(user?._id) ? (
+                                <></>
+                            ) : (
+                                <div className='flex items-center justify-center m-4'>
+                                    <button onClick={addHandler} className='bg-green-600 text-xl flex items-center justify-center transform hover:-translate-y-1 hover:scale-110'>
+                                        Add to cart
+                                    </button>
+                                </div>
+                            )
                         )}
                     </div>
                 </div>
