ef5188de09a268bbab95ca32c58cbd6b3e5bc83a
KokoKostov <127635232+KokoKostov@users.noreply.github.com>
2024-09-16 15:54:53 +0300

    adding lazy loads

--- ticket-web/src/App.js
+++ ticket-web/src/App.js
@@ -1,59 +1,76 @@
 
 import React, { Suspense } from "react";
-import Header from "./components/Header/Header";
-import ProtectedRoutes from "./utils/ProtectedRoutes"
 import  "./App.css"
-import CreateEvent from "./components/CreateEvent/CreateEvent";
+import ProtectedRoutes from "./utils/ProtectedRoutes"
+import { AuthProvider } from "./providers/AuthProvider";
+import { CartProvider } from "./providers/CartProvider";
 import { Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import CreateEvent from "./components/CreateEvent/CreateEvent";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import Details from "./components/Details/Details";
-import { AuthProvider } from "./providers/AuthProvider";
 import Home from "./components/Home/Home";
-// import Catalog from "./components/Catalog/Catalog";
 import Edit from "./components/Edit/Edit";
-import Profile from "./components/Profile/Profile";
-import { CartProvider } from "./providers/CartProvider";
-import Cart from "./components/Cart/Cart";
 
+const LazyCart = React.lazy(()=>import('./components/Cart/Cart'))
+const LazyDetails = React.lazy(()=>import('./components/Details/Details'))
+const LazyProfile = React.lazy(()=>import('./components/Profile/Profile'))
 const LazyCatalog = React.lazy(()=>import('./components/Catalog/Catalog'))
 
 
 function App() {
-
   return (
     <div className="App">
   
-      
       <AuthProvider>
         <CartProvider>
       <Header/>
-      <Routes>
-    
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/> }/>
-        <Route path="/register" element={<Register/> }/>
-        <Route path= "/tickets/:id" element={<Details/> }/>
-        <Route
-              path="/catalog"
-              element={
-                <Suspense fallback={<div>Loading...11</div>}>
-                  <LazyCatalog />
-                </Suspense>
-              }
-            />
+      <Routes>   
 
-        <Route element={<ProtectedRoutes/>}>
+        <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/> }/>
+          <Route path="/register" element={<Register/> }/>
+          <Route
+                path="/tickets/:id"
+                element={
+                  <Suspense fallback={<div>Loading...11</div>}>
+                    <LazyDetails />
+                  </Suspense>
+                }
+              />
+          <Route
+                path="/catalog"
+                element={
+                  <Suspense fallback={<div>Loading...11</div>}>
+                    <LazyCatalog />
+                  </Suspense>
+                }
+              />
+          <Route element={<ProtectedRoutes/>}>
           <Route path="/create" element={<CreateEvent/> }/>
-          <Route path="/profile/:id" element={<Profile/> }/>
-          <Route path='/cart' element={<Cart/>}/>
+          <Route
+                path="/profile/:id"
+                element={
+                  <Suspense fallback={<div>Loading...11</div>}>
+                    <LazyProfile />
+                  </Suspense>
+                }
+              />
+          <Route
+                path="/cart"
+                element={
+                  <Suspense fallback={<div>Loading...11</div>}>
+                    <LazyCart />
+                  </Suspense>
+                }
+              />
           <Route path="/tickets/edit/:id" element={<Edit/>}/>
         </Route>
-     
+
       </Routes>
       </CartProvider>
       </AuthProvider>
-     
     
     </div>
   );
